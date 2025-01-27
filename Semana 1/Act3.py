0412bda2468cd9093ea28200bf3903379bca402b
pedrouanl777 <pedro.cantumtj@gmail.com>
2025-01-26 19:43:41 -0600

    Act3.py

---
+++ Semana 1/Act3.py
@@ -0,0 +1,56 @@
+## Tarea de Programación Básica ~º~ Pedro Antonio Soto Cantu ##
+####Ejercicio 1####
+
+#¿Cuál es la función de la memoria secundaria en una computadora?
+# c) Almacenar información durante mucho tiempo, incluso después de ciclos de apagado y encendido
+
+####Ejercicio 2####
+# ¿Qué es un programa?
+# Respuesta: Son instrucciones almacenadas en la computadora.
+
+####Ejercicio 3####
+# ¿Cuál es la diferencia entre un compilador y un intérprete?
+# Respuesta: En primera parte, un compilador necesita un programa completo como fichero, luego ejecuta un proceso
+# para traducir el codigo fuente a código máquina, tras lo cual coloca ese código máquina resultante dentro de otro
+# fichero para su ejecución posterior. En cambio con un intérprete, lee códigos fuente de los programas e interpreta
+# sus instrucciones sobre la marcha.
+
+####Ejercicio 4####
+# ¿Cuál de los siguiente contiene "código máquina"?
+# a) Intérprete de Python
+
+####Ejercicio 5####
+# ¿Qué está mal en el siguiente código?
+# >>>primt "Hola, Mundo"
+# File "<stdin>", line 1
+# primt "Hola, mundo"
+# '
+# SyntaxError: invalid syntax
+# >>>
+## Respuesta: El "primt", en realidad es "print".
+
+####Ejercicio 6####
+# ¿En qué lugar del computador queda almacenada una variable, como en este caso "x", después de
+# ejecutar la siguiente línea de Python?:
+# x = 123
+# b) Memoria Principal
+
+####Ejercicio 7####
+# ¿Qué mostrará en pantalla el siguiente programa?:
+# x = 43
+# x = x + 1
+# print (x)
+# Respuesta: Mostrará el número 44.
+
+####Ejercicio 8####
+# Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana:
+# 1. Unidad central de procesamiento. La anticipación, obsesionado con el "¿Qué hago ahora?".
+# 2. Memoria principal. La Adaptación u decisión, dándole la información inmediata a la CPU.
+# 3. Memoria secundaria. El pensamiento o memoria.
+# 4. Dispositivo de entrada. Trabajo en equipo.
+# 5. Dispositivo de salida. La Comunicación (Respecto a lo que ofrece, pantalla, audifonos, etc.)
+
+####Ejercicio 9####
+# ¿Cómo puedes corregir un "Error de síntaxis"?
+# Respuesta: Buscando primeramente en el código y corregir el punto, línea y el carácter erróneo. 
+# Y así comprobando si persiste o ya no.
