f6e9c113486a0af366ea9378269f12a506556a77
pedrouanl777 <pedro.cantumtj@gmail.com>
2025-02-03 23:51:27 -0600

    V 1.0

--- Semana 2 Actividad 4/Problema7.py
+++ Semana 2 Actividad 4/Problema7.py
@@ -1,1 +1,11 @@
-# u.u
+# No sé si se refería a eso pero hay fe
+print('Ingrese un número')
+n1 = int(input(""))
+
+res = n1 % 2
+if res == 0:
+    print("Tu número es par")
+else:
+    print("Tu número es impar")
+
+    
