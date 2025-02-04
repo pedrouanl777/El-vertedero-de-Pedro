84b66da0aefb4a6ed1e53c543d65b5d62be89ca2
pedrouanl777 <pedro.cantumtj@gmail.com>
2025-02-03 23:22:52 -0600

    Pr6

--- Semana 2 Actividad 4/Problema6.py
+++ Semana 2 Actividad 4/Problema6.py
@@ -1,1 +1,21 @@
-# aaaa
+# Segun google esto se necesita para sacar el interés compuesto (?)
+print('Saldo actual')
+current = float(input())
+
+print('Adición anual')
+adicionAnual = float(input())
+
+print('Años a invertir')
+años = int(input())
+
+print('Interés deseado')
+interes = float(input())
+
+total = current
+
+for i in range(años):
+    total = total + adicionAnual
+    total = total + (interes * total / 100)
+    print(i)
+
+print('Resultado: $' + str(total)) # Toca practicar con más de estos :B
