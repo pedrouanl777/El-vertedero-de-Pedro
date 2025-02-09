7a4806912016af2d3e8d853c32bbc1c2f9d8aec1
pedrouanl777 <pedro.cantumtj@gmail.com>
2025-02-09 17:20:48 -0600

    9022502

---
+++ Semana 3/Problema 3.py
@@ -0,0 +1,15 @@
+print("Ingrese su moneda en pesos MX")
+n1 = float(input(""))
+
+USD = n1 * 0.050
+EUR = n1 * 0.047
+GBP = n1 * 0.039
+JPY = n1 * 7.71
+
+mensaje = f"""
+La conversión de su moneda a USD es: {USD} $
+La conversión de su moneda a EUR es: {EUR} $
+La conversión de su moneda a GBP es: {GBP} $
+La conversión de su moneda a JPY es: {JPY} $
+"""
+print(mensaje)
